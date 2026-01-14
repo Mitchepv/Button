f8331631d9f45687f094f93e246b60069b4a710a
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-14 12:08:33 -0500

    lesson 1.3 button, state, and Variables

--- Button/ContentView.swift
+++ Button/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a Programmer!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(Font.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button("Click me") {
+                message = "Awesome"
+            }
+               }
+               .padding()
     }
 }
 
