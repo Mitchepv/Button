256b9b60ae9fd422aa8d6346915f6fad2f51c1bb
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-15 14:28:30 -0500

    conditionals

--- Button/ContentView.swift
+++ Button/ContentView.swift
@@ -8,38 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
+    
     var body: some View {
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(systemName:imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(Font.largeTitle)
                 .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+
             
             Spacer()
             
-            HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
+            
+            Button("Press me!") {
+                let message1 = "You are Awesome!"
+                let message2 = " You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else{
+                    message = message1
+                    imageString = imageString1
                 }
-               
-                Button("Great!"){
-                       message = "Great!"
-                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(Font.title2)
             .tint(.orange)
         }
-            .padding()
+        .padding()
         
     }
 }
